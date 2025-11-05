c84d988c27fd07dda4b3ca2e8b00a08987f3ee05
GeorgeASDJ <Ggk2042005@mail.ru>
2025-11-05 14:47:13 +0300

    Implement my_vector

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "my_vector.h"
+
+int main() {
+    kp2024::vector<int> vec;
+    vec.push_back(1);
+    vec.push_back(2);
+    vec.push_back(3);
+
+    std::cout << "size: " << vec.size() << std::endl;
+    std::cout << "capacity: " << vec.capacity() << std::endl;
+
+    for (size_t i = 0; i < vec.size(); ++i) {
+        std::cout << "vector[" << i << "] = " << vec[i] << std::endl;
+    }
+
+    return 0;
+}
